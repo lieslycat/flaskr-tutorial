d1718e0333667ac94fd8130bf57b9149a4bb77ed
wanghui <wangh@ciicsh.com>
2018-06-21 17:57:40 +0800

    init project

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -19,3 +19,24 @@ def register():
 
 		if not username:
 			error = 'Username is required'
+		elif not password:
+			error = 'Password is required'
+		elif db.execute(
+			'SELECT id FROM user WHERE username = ?',
+			username
+		).fetchone() is not None:
+			error = 'User {} is already registered.'.format(username)
+		if not error:
+			db.execute(
+				'INSERT INTO user (username, password) VALUES(?, ?)',
+				(username, generate_password_hash(password))
+			)
+			db.commit()
+			return redirect(url_for('auth.login'))
+		flash(error)
+	return render_template('auth/register.html')
+
+@bp.route('/login', methods=('POST', 'GET'))
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
